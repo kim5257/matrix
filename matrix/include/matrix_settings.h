5b2147d0b3c05e59decd7ca489660cee2232c237
Jae-Gu Kim <kim5257@naver.com>
2015-02-16 14:18:34 +0900

    1. 누락된 파일 commit

---
+++ matrix/include/matrix_settings.h
@@ -0,0 +1,16 @@
+/*
+ * setting.h
+ *
+ *  Created on: 2015. 02. 09.
+ *      Author: asran
+ */
+
+#ifndef INCLUDE_MATRIX_SETTINGS_H_
+#define INCLUDE_MATRIX_SETTINGS_H_
+
+#define		PLATFORM_WINDOWS		(0)
+#define		PLATFORM_LINUX		(1)
+
+#define		PLATFORM		(PLATFORM_LINUX)
+
+#endif
